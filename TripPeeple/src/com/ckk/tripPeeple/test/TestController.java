d1a9c04f8f037b3a49cc01f47ecad44cea466ea6
kkangmo23 <hankyung055@hankyung055-PC>
2015-05-21 09:33:47 +0900

    test controller 추가

---
+++ TripPeeple/src/com/ckk/tripPeeple/test/TestController.java
@@ -0,0 +1,14 @@
+package com.ckk.tripPeeple.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController {
+
+	@RequestMapping(value = "/test.do", method = RequestMethod.GET)
+	public String viewTest() {
+		return "test/bHome02";
+	}
+}
